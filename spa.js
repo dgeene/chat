62c264e5329c6820ab00ab44f20b5b6b5048561c
dgeene <david.geene@gmail.com>
2016-02-27 09:23:43 -0500

    finish toggle function

--- spa.js
+++ spa.js
@@ -16,9 +16,32 @@ var spa = (function($)
         onClickSlider,
         initModule;
 
+
+
     // toggle slider height
     toggleSlider = function()
     {
+        var sliderHeight = $chatSlider.height();
+
+        // extend slider if retracted
+        if (slider_height === configMap.retracted_height)
+        {
+            $chatSlider
+                .animate( {height: configMap.extended_height} )
+                .attr( 'title', configMap.extended_title );
+            return true;
+        }
+
+        else if (slider_height === configMap.extended_height)
+        {
+            $chatSlider
+                .animate( {height: configMap.retracted_height} )
+                .attr( 'title', configMap.retracted_title);
+            return true
+        }
+
+        // dont take action if the slider is in transition
+        return false;
     };
 
     // receives click event and calls toggleSlider
